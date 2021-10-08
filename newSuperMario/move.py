8860a94782bd5669a038cfaccb7cbc342e762b93
JungRobbi <84313999+JungRobbi@users.noreply.github.com>
2021-10-09 04:50:17 +0900

    Mario moving

--- newSuperMario/move.py
+++ newSuperMario/move.py
@@ -4,6 +4,7 @@ from pico2d import *
 def handle_events():
     global running
     global dir
+    global herodir
 
     events = get_events()
     for event in events:
@@ -19,8 +20,10 @@ def handle_events():
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir -= 1
+                herodir = 1
             elif event.key == SDLK_LEFT:
                 dir += 1
+                herodir = -1
     pass
 
 open_canvas()
@@ -31,6 +34,7 @@ running = True
 x = 800 // 2
 
 dir = 0 # LEFT = -1 RIGHT = 1
+herodir = 1
 
 frame = 0
 speed = 0
@@ -38,9 +42,15 @@ framedir = 0
 
 while running:
     clear_canvas()
-
-
-    character.clip_draw(frame * 32, 960 - 40, 32, 40, x, 90)
+    if dir == 0:
+        if herodir == 1:
+            character.clip_draw(frame * 32, 960 - 40, 32, 40, x, 90)
+        else:
+            character.clip_composite_draw(frame * 32, 960 - 40, 32, 40, 0, 'h', x, 90, 32, 40)
+    elif dir == 1:
+        character.clip_draw(frame * 32, 960, 32, 40, x, 90)
+    elif dir == -1:
+        character.clip_composite_draw(frame * 32, 960, 32, 40, 0, 'h', x, 90, 32, 40)
 
     update_canvas()
 
@@ -52,7 +62,7 @@ while running:
 
     if framedir == 0:
         frame = frame + speed
-        if frame >= 8:
+        if frame >= 4:
             framedir = 1
     else:
         frame = frame - speed

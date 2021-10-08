efbcb83db105a8370efe45208a703c557ff52d95
JungRobbi <84313999+JungRobbi@users.noreply.github.com>
2021-10-09 04:27:00 +0900

    Mario PNG

--- newSuperMario/move.py
+++ newSuperMario/move.py
@@ -25,7 +25,7 @@ def handle_events():
 
 open_canvas()
 
-character = load_image('boo.png')
+character = load_image('MarioMove.png')
 
 running = True
 x = 800 // 2
@@ -34,18 +34,32 @@ dir = 0 # LEFT = -1 RIGHT = 1
 
 frame = 0
 speed = 0
+framedir = 0
 
 while running:
     clear_canvas()
 
-    character.clip_draw(0, (frame+3) * 32, 32, 32, x, 90)
+
+    character.clip_draw(frame * 32, 960 - 40, 32, 40, x, 90)
 
     update_canvas()
 
     handle_events()
 
+
     speed = (speed + 1) % 2
-    frame = (frame + speed) % 3
+    # frame = (frame + speed) % 8
+
+    if framedir == 0:
+        frame = frame + speed
+        if frame >= 8:
+            framedir = 1
+    else:
+        frame = frame - speed
+        if frame == 0:
+            framedir = 0
+
+
     x += dir * 5
     delay(0.04)
 
